8866fce2c9713740fb17ff6b237969ea8fb93d29
komal shrestha <komalstha0000.com>
2025-02-21 13:40:24 +0545

    Add authentication middleware to admin routes and clean up app.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::match(["GET","POST"],'login',[LoginController::class,'login'])->name('login');
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::prefix('admin')->name('admin.')->group(function(){
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
     Route::get('',[DashboardController::class,'index'])->name('index');
 
     Route::prefix('speciality')->name('speciality.')->group(function(){
