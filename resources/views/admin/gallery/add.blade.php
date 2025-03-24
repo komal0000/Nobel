a5210fd74e706ccca41d6e81aea10790ff604d92
komal shrestha <komalstha0000.com>
2025-03-24 14:36:26 +0545

    Refactor gallery add form layout for improved spacing; recreate internship template view

--- resources/views/admin/gallery/add.blade.php
+++ resources/views/admin/gallery/add.blade.php
@@ -9,25 +9,25 @@
         @csrf
         <div class="row">
             <div class="col-md-4">
-                <div class="col-md-12">
+                <div class="col-md-12 mb-2">
                     <label for="image">Image <span style="color: red;">*</span></label>
                     <input type="file" name="image" id="image" class="form-control dropify" required>
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="col-md-12">
+                <div class="col-md-12 mb-2">
                     <label for="title">Title </label>
                     <input type="text" name="title" id="title" class="form-control">
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-12 mb-2">
                     <label for="description">Description </label>
                     <textarea name="description" id="description" class="form-control"></textarea>
                 </div>
-            </div>
-            <div class="col-md-4 d-flex align-items-end">
-                <button class="btn btn-primary">
-                    Save
-                </button>
+                <div class="col-md-4 d-flex align-items-end">
+                    <button class="btn btn-primary">
+                        Save
+                    </button>
+                </div>
             </div>
         </div>
     </form>
