194f173c2aa9b1a71430b968686b61c1e79a9029
komal shrestha <komalstha0000.com>
2025-03-27 18:20:35 +0545

    Add national section to home page: include national view in front index.

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -8,6 +8,7 @@
     @include('front.pages.home.team')
     @includeIf('front.cache.home.care')
     @includeIf('front.cache.home.videos')
+    @includeIf('front.pages.home.national')
     @includeIf('front.cache.home.updates')
     @includeIf('front.cache.home.awards')
     @includeIf('front.cache.home.newsEvent')
