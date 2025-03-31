0f660c15dac42a370e059374422798e4b483b11a
komal shrestha <komalstha0000.com>
2025-03-31 12:53:00 +0545

    Refactor header and top banner styles for improved layout and responsiveness; adjust padding, margins, and image sizes.

--- resources/views/admin/template/home/slider.blade.php
+++ resources/views/admin/template/home/slider.blade.php
@@ -1,12 +1,11 @@
 
-<div class="top-banner">
-
+<div id="home-top-banner">
     <div class="top-banner-slider">
         @foreach ($sliders as $slider)
         <div class="image-card">
             <picture>
                 <source media="(min-width: 768px)" srcset="{{ Storage::url($slider->desktop_image) }}">
-                <source media="(max-width: 767px)" srcset="{{ Storage::url($slider->mobile_image) }}">
+                <source media="(max-width: 768px)" srcset="{{ Storage::url($slider->mobile_image) }}">
                 <img class="img-fluid" src="{{Storage::url($slider->desktop_image) }}" alt="Slider Image">
             </picture>
         </div>
