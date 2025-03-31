0254c127031ec0ffc11b8ddca3ad0162ab98514d
komal shrestha <komalstha0000.com>
2025-03-31 16:11:30 +0545

    Refactor find-doc component; update layout and improve speciality selection functionality

--- resources/views/admin/template/home/teams.blade.php
+++ resources/views/admin/template/home/teams.blade.php
@@ -1,16 +1,18 @@
 <div class="find-doc">
     <form>
         <div class="find-doc-form">
-            <div class="select-wrap d-flex justify-content-center gap-2 mb-4">
+            <div class="select-wrap d-flex justify-content-center gap-2" style="margin-bottom: 20px;">
                 <div class="find-doc-speciality-wrap">
-                    <div class="default-speciality-wrap d-flex justify-content-between gap-5 cursor-pointer" onclick="showList()">
+                    <div class="default-speciality-wrap d-flex justify-content-between gap-5"
+                        id="default-speciality-wrap">
+                        <span class="default-speciality-item d-inline-block text-truncate">All
+                            Specialities</span>
                         <div class="position-relative">
-                            <span class="anchor-down-btn d-inline-block border-dark"></span>
+                            <span class="anchor-down-btn d-inline-block" style="border-color: #000"></span>
                         </div>
                     </div>
-                    <div class="list-wrap" id="list-wrap" style="display: none;">
-                        <input type="text" id="speciality-search" placeholder="Search Specialities..." class="form-control mb-2">
-                        <ul class="find-doc-list-speciality" id="find-doc-speciality" style="overflow: unset;">
+                    <div class="list-wrap" id="list-wrap">
+                        <ul class="find-doc-list-speciality" id="find-doc-speciality">
                             @foreach ($specialities as $specialty)
                                 <li data-value="{{ $specialty->id }}">{{ $specialty->title }}</li>
                             @endforeach
