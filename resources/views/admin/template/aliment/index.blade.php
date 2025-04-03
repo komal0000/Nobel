d850136bc4c979f8f99ad20c4d213a136101675f
komal shrestha <komalstha0000.com>
2025-04-03 16:06:29 +0545

    Update aliment index view to display short description directly in card component

--- resources/views/admin/template/aliment/index.blade.php
+++ resources/views/admin/template/aliment/index.blade.php
@@ -1,8 +1,8 @@
 <x-allPage title="All Ailments">
     @foreach ($aliments as $aliment)
         <div class="col-md-6 each-card">
-            <x-card class="bg-white rounded-4" title="{{ $aliment->title }}" slot="{{ $aliment->short_description }}"
-                href="{{ route('aliment.single', $aliment->id) }}">
+            <x-card class="bg-white rounded-4" title="{{ $aliment->title }}" slot="" href="{{ route('aliment.single', $aliment->id) }}">
+                {{ $aliment->short_description }}
             </x-card>
         </div>
     @endforeach
