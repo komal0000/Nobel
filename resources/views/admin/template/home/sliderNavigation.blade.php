1191fa6cd4722063a2220e39b6977b2139dd58a8
komal shrestha <komalstha0000.com>
2025-04-03 12:32:20 +0545

    Refactor slider navigation: remove unnecessary floating-tab class from swiper-wrapper for cleaner markup

--- resources/views/admin/template/home/sliderNavigation.blade.php
+++ resources/views/admin/template/home/sliderNavigation.blade.php
@@ -1,7 +1,7 @@
 <div class=" slide-list d-flex">
-    <div class="swiper-wrapper floating-tab d-flex" id="slick-slider">
+    <div class="swiper-wrapper floating-tab" id="slick-slider">
         @foreach ($navigations as $navigation)
-            <a href="{{ $navigation->link }}" class="swiper-slide left-hover" target="_blank">
+            <a href="{{ $navigation->link }}" class="swiper-slide">
                 <img src="{{ Storage::url($navigation->icon) }}" class="me-2" alt="Appointment Image">
                 <span>{{ $navigation->title }}</span>
             </a>
