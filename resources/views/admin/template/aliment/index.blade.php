43e381a122ca0bf746993b07ad05e430a23755ec
komal shrestha <komalstha0000.com>
2025-04-03 14:59:27 +0545

    Update aliment index view to use short description in card slot

--- resources/views/admin/template/aliment/index.blade.php
+++ resources/views/admin/template/aliment/index.blade.php
@@ -1,8 +1,8 @@
 <x-allPage title="All Ailments">
     @foreach ($aliments as $aliment)
         <div class="col-md-6 each-card">
-            <x-card class="bg-white rounded-4" title="{{ $aliment->title }}" href="{{ route('aliment.single', $aliment->id) }}">
-                {{ $aliment->description }}
+            <x-card class="bg-white rounded-4" title="{{ $aliment->title }}" slot= "{{ $aliment->short_description }}"
+                href="{{ route('aliment.single', $aliment->id) }}">
             </x-card>
         </div>
     @endforeach
