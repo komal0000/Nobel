ba945377cb25255038a6afdff8df7f53ce398a86
komal shrestha <komalstha0000.com>
2025-04-03 14:25:01 +0545

    Remove commented-out sections for ailment overview and speciality ailments in blade templates

--- resources/views/admin/template/aliment/single.blade.php
+++ resources/views/admin/template/aliment/single.blade.php
@@ -38,33 +38,6 @@
         </form>
     </div>
 </section>
-{{-- <section id="ailment-overview">
-    <div class="main-container d-block d-md-flex">
-        <div class="heading mb-4">
-            What Is an {{$aliment->title}}?
-        </div>
-        <div class="content-wrapper">
-            <p class="para-wrap collapsed">
-                The Achilles tendon is the largest and strongest tendon in the human body. It can be found at the
-                posterior of your ankle and links your heel bone to your calf muscles. By enabling you to move your foot
-                and flex your toes downwards, it plays a crucial role in movement.
-
-                The Achilles tendon is susceptible to injury when it is pulled, overstretched, or ruptured due to rapid
-                jerky motions such as jumping or pivoting, or repetitive stress such as running extended distances.
-
-                If you have a suspicion of Achilles tendon injury, it is crucial to quickly look for medical aid. The
-                physician may advise utilising the RICE method, which involves rest, ice, compression, and elevation to
-                alleviate inflammation and pain. Occasionally, surgical intervention or physical therapy might be
-                required.
-
-                It is crucial to take preventive measures against Achilles tendon injuries, such as adequately warming
-                up and stretching before any physical activity, donning suitable footwear, and gradually intensifying
-                your exercise routine.
-            </p>
-            <button class="read-more-btn">Read More <i class="bi bi-chevron-down"></i></button>
-        </div>
-    </div>
-</section> --}}
 <section id="ailment-content">
     <div class="main-container">
        <div class="scroll-anchor-links">
@@ -72,4 +45,3 @@
        </div>
     </div>
  </section>
-w
