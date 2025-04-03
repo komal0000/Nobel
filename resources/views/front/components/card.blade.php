c763c0d1a5b34a50e36084baba91130ac59b231d
komal shrestha <komalstha0000.com>
2025-04-03 15:55:50 +0545

    Refactor card component to include slot for dynamic content and improve layout consistency

--- resources/views/front/components/card.blade.php
+++ resources/views/front/components/card.blade.php
@@ -1,11 +1,11 @@
-@props(['class' => '', 'image' => '', 'title' => '', 'btn' => 'Know More', 'href' =>''])
+@props(['class' => '', 'image' => '', 'title' => '', 'slot' => '', 'btn' => 'Know More', 'href' => ''])
 <div class="d-flex flex-column flex-xl-row-reverse main-card {{ $class }}">
-    @if($image)
+    @if ($image)
         <div class="img-wrapper">
             <img src="{{ $image }}" alt="{{ $title ?? 'Card Image' }}" class="img-fluid">
         </div>
     @endif
-    <div class="body {{ !$image ? 'full-width' : ''}}">
+    <div class="body {{ !$image ? 'full-width' : '' }}">
         <h3 class="title heading-md">{{ $title }}</h3>
         <p class="para-wrap content">{{ $slot }}</p>
         <div class="d-flex justify-content-between know-btn">
