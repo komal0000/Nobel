f51df02444233beb9ca382091c55b222d631f5a4
Amrit Rijal <rijalamrit720@gmail.com>
2025-06-25 14:27:32 +0545

    refactor: remove subHeading from type components for cleaner presentation

--- resources/views/admin/template/speciality/single/aliment.blade.php
+++ resources/views/admin/template/speciality/single/aliment.blade.php
@@ -1,5 +1,5 @@
 <section id="ailment" class="ailment" data-content="Ailment">
     <div class="main-container">
-        <x-type2 heading="Ailments" subHeading="Team approach and compassionate care for holistic heart health" :items="$specialityAliments"> </x-type2>
+        <x-type2 heading="Ailments" :items="$specialityAliments"> </x-type2>
     </div>
 </section>
--- resources/views/admin/template/speciality/single/treatment.blade.php
+++ resources/views/admin/template/speciality/single/treatment.blade.php
@@ -1,5 +1,5 @@
 <section id="treatment" class="treatment" data-content="Treatment">
     <div class="main-container">
-        <x-type2 heading="Treatments" subHeading="Team approach and compassionate care for holistic heart health" :items="$specialityTreatment"> </x-type2>
+        <x-type2 heading="Treatments" :items="$specialityTreatment"> </x-type2>
     </div>
 </section>
