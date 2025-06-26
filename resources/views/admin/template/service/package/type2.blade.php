2a1ac42ff3dc116b7c12f2a565c6b12383940cd5
Amrit Rijal <rijalamrit720@gmail.com>
2025-06-26 07:31:30 +0545

    improved css of type 2 package

--- resources/views/admin/template/service/package/type2.blade.php
+++ resources/views/admin/template/service/package/type2.blade.php
@@ -3,18 +3,29 @@
         <div class="heading text-center mb-4">
             Our Packages
         </div>
+        @php
+            $count = count($packages2);
+            $width = '100%';
+            if ($count == 2) {
+                $width = '50%';
+            } elseif ($count == 3) {
+                $width = '33.3333%';
+            } elseif ($count == 4) {
+                $width = '25%';
+            }
+        @endphp
         <div class="package-list">
             <div class="package-slider">
                 @foreach ($packages2 as $package)
-                    <div class="each-package">
-                        <div class="heading-sm">{{$package->title}}</div>
+                    <div class="each-package" style="min-width: {{ $width }}">
+                        <div class="heading-sm">{{ $package->title }}</div>
                         <div class="package-body">
                             <ul>
                                 {!! $package->description !!}
                             </ul>
                         </div>
                         <div class="package-price">
-                            <div class="price">Rs. {{$package->price}}</div>
+                            <div class="price">Rs. {{ $package->price }}</div>
                         </div>
                     </div>
                 @endforeach
