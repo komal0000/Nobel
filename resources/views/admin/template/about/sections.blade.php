676672fe4759b6df5a62b9dcd540e5049a8cf11f
Amrit Rijal <rijalamrit720@gmail.com>
2025-06-29 10:28:29 +0545

    refactor: remove unused about-us banner section for cleaner code

--- resources/views/admin/template/about/sections.blade.php
+++ resources/views/admin/template/about/sections.blade.php
@@ -1,13 +1,3 @@
-<section id="about-us-banner">
-    <picture class="img-wrap">
-        <source media="(min-width: 768px)" srcset="{{ asset('front/assets/img/about-us/banner.jpg') }}" width="1920"
-            height="500" alt="About Us">
-        <source media="(min-width: 320px)" srcset="{{ asset('front/assets/img/about-us/banner-md.jpg') }}" width="480"
-            height="320" alt="About Us">
-        <img src="{{ asset('front/assets/img/about-us/banner.jpg') }}" class="img-fluid w-100 object-fit-cover h-100"
-            alt="About Us">
-    </picture>
-</section>
 @foreach ($abouts as $about)
     {{-- <section id="about-nobel">
         <div class="main-container">
