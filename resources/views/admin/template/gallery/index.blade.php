c43f9129b2f7418f39861296e439c4e5749d5964
Amrit Rijal <rijalamrit720@gmail.com>
2025-07-04 12:09:42 +0545

    fix: adjust gallery card image dimensions and layout for better responsiveness

--- resources/views/admin/template/gallery/index.blade.php
+++ resources/views/admin/template/gallery/index.blade.php
@@ -1,6 +1,6 @@
 <section id="galleries">
    <div class="main-container">
-      <div class="row">
+      <div class="row g-3">
          @foreach ($galleryTypes as $type)
          <div class="col-md-6 col-lg-4 col-xl-3">
             <a href="{{route('gallery.single', ['slug' => $type->slug])}}" class="gallery-card">
