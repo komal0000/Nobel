7b7b9d31edbdc2e8c70e17cf8497d0d0ee0afdb7
Amrit Rijal <rijalamrit720@gmail.com>
2025-07-06 11:03:24 +0545

    refactor: remove unused color options from color scheme settings

--- resources/views/admin/setting/colorscheme.blade.php
+++ resources/views/admin/setting/colorscheme.blade.php
@@ -54,24 +54,6 @@
             </div>
 
             <div class="col-md-4 form-group">
-                <input type="color" id="color-dark-3" name="color-dark-3"
-                    value="{{ $oldData['color-dark-3'] ?? '#a83722' }}">
-                <label for="color-dark-3">--color-dark-3</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="color-dark-4" name="color-dark-4"
-                    value="{{ $oldData['color-dark-4'] ?? '#902f1d' }}">
-                <label for="color-dark-4">--color-dark-4</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="color-dark-5" name="color-dark-5"
-                    value="{{ $oldData['color-dark-5'] ?? '#782718' }}">
-                <label for="color-dark-5">--color-dark-5</label>
-            </div>
-
-            <div class="col-md-4 form-group">
                 <input type="color" id="color-light-1" name="color-light-1"
                     value="{{ $oldData['color-light-1'] ?? '#f26045' }}">
                 <label for="color-light-1">--color-light-1</label>
@@ -81,24 +63,6 @@
                 <input type="color" id="color-light-2" name="color-light-2"
                     value="{{ $oldData['color-light-2'] ?? '#f37159' }}">
                 <label for="color-light-2">--color-light-2</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="color-light-3" name="color-light-3"
-                    value="{{ $oldData['color-light-3'] ?? '#f5836e' }}">
-                <label for="color-light-3">--color-light-3</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="color-light-4" name="color-light-4"
-                    value="{{ $oldData['color-light-4'] ?? '#f69583' }}">
-                <label for="color-light-4">--color-light-4</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="color-light-5" name="color-light-5"
-                    value="{{ $oldData['color-light-5'] ?? '#f8a798' }}">
-                <label for="color-light-5">--color-light-5</label>
             </div>
 
             <div class="col-md-4 form-group">
@@ -124,30 +88,7 @@
                 <label for="color-dark-grey">--color-dark-grey</label>
             </div>
 
-            <div class="col-md-4 form-group">
-                <input type="color" id="secondary-white" name="secondary-white"
-                    value="{{ $oldData['secondary-white'] ?? '#f6f6f6' }}">
-                <label for="secondary-white">--secondary-white</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="border-color" name="border-color"
-                    value="{{ $oldData['border-color'] ?? '#b7b7b7' }}">
-                <label for="border-color">--border-color</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="color" id="border-color-light" name="border-color-light"
-                    value="{{ $oldData['border-color-light'] ?? '#eee' }}">
-                <label for="border-color-light">--border-color-light</label>
-            </div>
-
-            <div class="col-md-4 form-group">
-                <input type="text" id="font-main" name="font-main"
-                    value="{{ $oldData['font-main'] ?? '\"Montserrat\", sans-serif' }}">
-                <label for="font-main">--font-main</label>
-            </div>
-            <div class="col-md-4 form-group">
+            <div class="col-md-12 form-group">
                 <button class="btn btn-primary">Save Data</button>
             </div>
         </div>
