f03ec098ec3a80a309718aafac90c36f763f0fea
Amrit Rijal <rijalamrit720@gmail.com>
2025-07-07 09:39:48 +0545

    irc design update

--- resources/views/admin/setting/template/irc.blade.php
+++ resources/views/admin/setting/template/irc.blade.php
@@ -1,6 +1,6 @@
 <section id="irc-nobel">
    <div class="main-container">
-      <div class="heading mb-4 text-center">{{$irc->title}}</div>
+      <div class="heading">{{$irc->title}}</div>
       <p class="para-wrap">
          {!! $irc->description !!}
      </p>
